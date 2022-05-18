74f943ae3c6147092275f70a2becdc758dadf3f9
gesht1 <gebriegashaw7@gmail.com>
2022-05-18 08:50:52 +0300

    firsit commit

---
+++ component/Featured.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Featured = () => {
+  return (
+    <div className={styles.container}>
+      <Image src="/image/arrowl.png" alt="" />
+      <div className={styles.wrapper}>
+        <div className={styles.Imagecontainer}>
+          
+        </div>
+      </div>
+      <Image src="/image/arrowr.png" alt="" />
+    </div>
+  );
+};
+export default Featured;
